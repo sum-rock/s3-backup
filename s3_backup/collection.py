2185b199b2ae7287bc5f14efd4746c6d20bfb590
august <sum-rock@rockyhillhouse.org>
2022-05-17 19:42:59 -0500

    Removed flags from clean function
    Extra clean isn't a thing and force should just be default. KISS.

--- s3_backup/collection.py
+++ s3_backup/collection.py
@@ -15,11 +15,7 @@ class Collection:
         cmd = f"duplicity list-current-files {self.profile.s3_url}"
         os.system(cmd)
 
-    def do_cleanup(self, extra_clean, force):
-        cmd = "duplicity cleanup "
-        if extra_clean:
-            cmd += "--extra-clean "
-        if force:
-            cmd += "--force "
+    def do_cleanup(self):
+        cmd = "duplicity cleanup --force"
         cmd += self.profile.s3_url
         os.system(cmd)
